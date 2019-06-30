d1e07a4c0633de2400b8f1eb80ab8bc75e6d4fe7
Doktatech <Doktatech2@gmail.com>
2019-06-30 17:37:46 +0300

    Create URL pattern for the profile API

--- award/urls.py
+++ award/urls.py
@@ -19,7 +19,7 @@ urlpatterns=[
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
     url(r'^api/projects/$', views.ProjectList.as_view()),
-    url(r'^api/profiles',views.ProfileList.as_view()),
+    # url(r'^api/profiles',views.ProfileList.as_view()),
     url(r'^api/profile/(?P<pk>\d+)',views.ProfileDesc.as_view())
 ]
 if settings.DEBUG:
