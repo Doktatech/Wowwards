b6f7c92863f29a630a5e7b16efb6aaa39c4ac4a2
Doktatech <Doktatech2@gmail.com>
2019-06-30 20:30:11 +0300

    Create the token generation class

--- award/tokens.py
+++ award/tokens.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.utils import six
+# Token generator class for our APIs
 class TokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
         return (
