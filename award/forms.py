d805e37b78a4d72b3dce47a60741edf9219bc5cd
Doktatech <Doktatech2@gmail.com>
2019-06-30 10:00:37 +0300

    Create Sign up form for users

--- award/forms.py
+++ award/forms.py
@@ -10,11 +10,11 @@ class ProjectForm(forms.ModelForm):
         
 
 
-# class SignupForm(UserCreationForm):
-#     email = forms.EmailField(max_length=200, help_text='Required')
-#     class Meta:
-#         model = User
-#         fields = ('username', 'email', 'password1', 'password2')
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, help_text='Required')
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
 
 # class ReviewForm(forms.ModelForm):
 #     class Meta:
