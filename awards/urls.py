c73d1d896669255dc21b6623a586616922044282
Doktatech <Doktatech2@gmail.com>
2019-06-30 20:28:50 +0300

    Add API token URL

--- awards/urls.py
+++ awards/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     url(r'',include('award.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^logout/$', views.logout, {"next_page": '/'}), 
-    url(r'^api/tokens',obtain_auth_token)
+    url(r'^api/tokens',obtain_auth_token),
 ]
